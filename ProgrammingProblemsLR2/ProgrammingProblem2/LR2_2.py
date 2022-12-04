da7a10012ab7a718c15aee8028bc0d0ef06edb28
John Carlo Biag <119016862+JohnCarloBiag@users.noreply.github.com>
2022-12-04 10:28:28 +0800

    LR2_2 Source Code
    Working Source code for LR2_2

--- ProgrammingProblemsLR2/ProgrammingProblem2/LR2_2.py
+++ ProgrammingProblemsLR2/ProgrammingProblem2/LR2_2.py
@@ -4,3 +4,26 @@ loop in which it prints the number of lines in the file and prompts the user for
 Otherwise, the program prints the line associated with that number.
 
 """
+enterfile = input("Enter the file name: ")
+file = open(enterfile, 'r')
+linecount = 0
+
+for line in file:
+    linecount = linecount + 1
+
+print("Total Number of lines in the file: ", linecount)
+
+while True:
+    linenum = 0
+
+    num = int(input("Enter Line Number you want to use or press 0 to quit: "))
+    if num >=1 and num <= linecount:
+        file = open(enterfile, 'r')
+        for lines in file:
+            linenum = linenum + 1
+            if linenum == num:
+                print(lines)
+    else:
+        if num == 0:
+            print("Thanks for using the program")
+            break
